76a763efec9be764ba7b7b2af6ba1ba43e358ba7
liujingdong <1522527313@qq.com>
2017-06-27 14:21:14 +0800

    删除文件夹和文件

--- src/android/DeleteFilePlugin.java
+++ src/android/DeleteFilePlugin.java
@@ -13,6 +13,7 @@ import java.io.File;
  * Created by CrazyDong on 2017/6/20.
  */
 public class DeleteFilePlugin extends CordovaPlugin {
+  private File mFile;
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -22,7 +23,8 @@ public class DeleteFilePlugin extends CordovaPlugin {
     * */
     if(action.equals("delete")){
       //删除sd卡args.getString(0)下的所有文件
-      boolean flagDelete = deleteAllFiles(new File(Environment.getExternalStorageDirectory() + args.getString(0)));
+      mFile = new File(Environment.getExternalStorageDirectory() + args.getString(0));
+      boolean flagDelete = deleteAllFiles(mFile);
       if(flagDelete){
         callbackContext.success("删除成功");
       }else {
@@ -67,14 +69,32 @@ public class DeleteFilePlugin extends CordovaPlugin {
 //
 //    }
 
-  static boolean deleteAllFiles(File file){
-    if(file.exists()){
-      file.delete();
-      return true;
-    }else{
-      return false;
-    }
+//  static boolean deleteAllFiles(File file){
+//    if(file.exists()){
+//      file.delete();
+//      return true;
+//    }else{
+//      return false;
+//    }
+//
+//  }
 
-  }
+
+      static boolean deleteAllFiles(File dir){
+        if(dir == null || !dir.exists() || !dir.isDirectory()){
+          return false;
+        }else{
+          for (File file : dir.listFiles()) {
+            if(file.isFile()){
+              file.delete();
+            }else if(file.isDirectory()){
+              deleteAllFiles(dir);
+            }
+          }
+          dir.delete();
+          return true;
+        }
+
+    }
 
 }
